b04948ce7a8c6140123383347e9f6c064737a81a
guidemoc <guidemoc@yahoo.com.br>
2022-12-25 11:26:17 -0300

    second commit

--- matrix.js
+++ matrix.js
@@ -10,13 +10,7 @@ c.width = window.innerWidth;
 
 // letters used on Matrix rain
 // more on: https://bit.ly/3yFJoU3
-const letters = [
-  "Ha",
-  "Haha",
-  "HaHa",
-  "Haha!",
-  "Ha!",
-];
+const letters = ["Ha", "Haha", "HaHa", "Haha!", "Ha!"];
 
 const fontSize = 18;
 
@@ -31,35 +25,25 @@ for (let x = 0; x < columns; x++) drops[x] = 1;
 
 //drawing the characters
 function draw() {
-  // painting the background
-  // the trick here is to use opacity
-  // so it will fade out with time
   ctx.fillStyle = "rgba(197, 197, 197, 0.05)";
   ctx.fillRect(0, 0, c.width, c.height);
 
-  // defining font styles (color and font family/size)
-  ctx.fillStyle = "#ab5dee";
   ctx.font = `${fontSize}px arial`;
 
-  // drawing the characters
   for (let i = 0; i < drops.length; i++) {
-    // get a random letter from the array
     const text = letters[Math.floor(Math.random() * letters.length)];
 
-    // drawing the character
+    if (i % 2 == 0) ctx.fillStyle = "#ab5dee";
+    else ctx.fillStyle = "#1da11f";
+
     ctx.fillText(text, i * fontSize, drops[i] * fontSize);
 
-    // moving the drop to the beginning of the canvas when it reaches the bottom
-    // or when the random expression is true in order to make the rain randomly
-    if (drops[i] * fontSize > c.height && Math.random() > 0.975)
-      drops[i] = 0;
+    if (drops[i] * fontSize > c.height && Math.random() > 0.975) drops[i] = 0;
 
-    // moving the drop in the Y coordinate
     drops[i]++;
   }
 
-  // recursive call to draw frame by frame
   window.requestAnimationFrame(draw);
 }
 
-draw()
+draw();
